12cb3b765b58e59a66e9f03a4c9a663b7565cb28
Dmitry Ageev <ageev.dmitry.s@gmail.com>
2022-09-14 01:03:11 +0300

    Optimize imports

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask
-from ask_sdk_core.skill_builder import SkillBuilder
-from flask_ask_sdk.skill_adapter import SkillAdapter
+import string
 
-from ask_sdk_core.utils import is_request_type, is_intent_name, get_slot_value
 from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.utils import is_request_type, is_intent_name, get_slot_value
 from ask_sdk_model import Response
 from ask_sdk_model.ui import SimpleCard
-import string
+from flask import Flask
+from flask_ask_sdk.skill_adapter import SkillAdapter
+
 import MicroConnect
 
 sb = SkillBuilder()
